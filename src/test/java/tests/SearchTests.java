aabcafaf947ae6fc56eb31dc1301546316aa1a84
MataAlieva <mataalieva58@gmail.com>
2026-01-02 18:21:59 +0300

    add negativeOpenArticleTest

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -2,7 +2,9 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -21,19 +23,20 @@ public class SearchTests extends TestBase {
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
+
     @Test
-    void openArticleTest() {
-        step("Поиск статьи", () -> {
+    void negativeOpenArticleTest() {
+        step("Поиск несуществующей статьи", () -> {
             $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("zxczxczxczxc");
         });
 
-        step("Клик по первой статье в результатах", () -> {
-            $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click();
+        step("Проверка, что результатов нет", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(size(0));
         });
 
-        step("Проверка, что статья открылась", () -> {
-            $(id("org.wikipedia.alpha:id/view_wiki_res_text")).shouldBe(visible);
+        step("Проверка, что экран статьи не открылся", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_res_text")).shouldNot(exist);
         });
     }
 }
